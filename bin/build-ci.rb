0a9699454ed35691875865bf11db82a75cf21165
gabbo <novelli.g@diffusionetessile.it>
2021-12-01 15:45:55 +0100

    aggiunta cartella container per gestire container dell'app

--- bin/build-ci.rb
+++ bin/build-ci.rb
@@ -21,7 +21,7 @@ class Project
     @name = name
   end
 
-  ALL = %w[api backend core frontend emails sample].map(&method(:new)).freeze
+  ALL = %w[api backend core frontend emails sample spree_line_comments].map(&method(:new)).freeze
   CORE_GEMS = %w[api core].freeze
 
   # Install subproject
